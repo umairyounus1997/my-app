259cc46aee5c8adebfa4f4abf88692207c7f121b
Umair Younus <41501844+umairyounus1997@users.noreply.github.com>
2020-05-31 13:12:27 +0500

    My first React App

---
+++ src/App.js
@@ -0,0 +1,27 @@
+// import React, {Component} from "react";
+// import Student from "./student";
+
+// // class App extends Component{
+// //     render(){
+// //         return (
+// //         <React.Fragment>
+// // <h1> Hello Karachi</h1>
+// // <h2>Hello Pakistan</h2>
+// // <h3>My First App</h3>
+// // <h3> Today is a very hottest Day</h3>
+        
+// //         </React.Fragment>
+// //         );
+// //     }
+// // }
+// const App = ()=>{
+//     return (
+//         <div>
+//             <Student/>
+//             </div>
+//     )
+// }
+
+// export default App;
+
+//props
