34552678022ccd2c212596671181b7ae9d4d3408
Umair Younus <41501844+umairyounus1997@users.noreply.github.com>
2020-05-31 20:34:15 +0500

    Final

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,47 @@
+// import React  from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App"
+
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+// // Let el = React.createElement('h1', null, "Hello ");
+// // let el = <h1> Welcome to My first App</h1>;
+// // ReactDOM.render(el, document.getElementById("root"));
+
+
+//props
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import Student from "./student";
+// ReactDOM.render(
+// <Student name = " Umair" roll={101}/>,
+// document.getElementById("root"));
+
+//Children in JSX
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import Student from "./student";
+
+// //Rendering Component
+// ReactDOM.render(<Student> I am Student </Student>, document.getElementById("root"));
+
+//State
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Student from "./student";
+
+//Rendering Component (State)
+// ReactDOM.render(<Student roll="013"/>, document.getElementById("root"))
+
+//Event Handling
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Student from './student';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//Rendering Component (Event Hanling)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Student roll="101"/>, document.getElementById("root"));
+
+
---
+++ my-app/src/student.js
@@ -0,0 +1,153 @@
+// import React from "react"
+
+
+// //Old Method (Do not Preferable)
+// // function Student(){
+// //     return <h1> Hello Ali</h1>
+// // }
+// const Student = () =>{
+//     return <h1>Hello Umair</h1>;
+// }
+
+// export default Student;
+
+//props
+// import React, { Component } from "react";
+// import PropTypes from "prop-types";
+
+// const Student = props =>{
+//     return(
+//     <div>
+//         <h1>Hello, {props.name}</h1>
+//     <h1>Your Profile No: {props.roll}</h1>
+//         </div>
+//     );
+// };
+
+// class Student extends Component{
+//     render(){
+//         return(
+//             <div>
+//                 <h1>Hello{this.props.name}</h1>
+//                 <h2>Your Roll No.{this.props.roll}</h2>
+//                 </div>
+//         );
+//     }
+// }
+// Student.propTypes = {
+
+//     name: PropTypes.string.isRequired
+// }; 
+// export default Student;
+
+// Children in JSX
+
+// import React, {Component} from "react";
+
+//Functional Component
+// const Student = props => {
+// return <h2> Hello World </h2>;
+// };
+
+// class Student extends Component {
+//     render(){
+//     return <h2> Hello World {this.props.children} </h2>;
+//     }
+// }
+
+// export default Student;
+
+
+//State
+// import React, { Component } from 'react';
+
+// class Student extends Component{
+//     state = {
+//         name: "Umair",
+//         roll: this.props.roll
+
+//     }
+//     render(){
+//         return(
+//         <h1>Hello {this.state.name} Your Roll No. {this.state.roll}</h1>
+        
+//         )
+//     }
+// }
+// export default Student;
+
+//State (Inside the Constructor)
+
+// import React, { Component } from 'react';
+
+// class Student extends Component{
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//                     name: "Umair",
+//                     roll: this.props.roll
+            
+//                 };
+//     }
+//     render(){
+//         return(
+//         <h1>Hello {this.state.name} Your Roll No. {this.state.roll}</h1>
+        
+//         )
+//     }
+// }
+// export default Student;
+
+//Event Handling
+
+// import React, { Component } from 'react';
+// class Student extends Component{
+// constructor(props){
+//     super(props);
+//     this.state = {
+//         name: "Umair",
+//         roll: this.props.roll
+//     };
+//     this.handleClick = this.handleClick.bind(this);
+// }
+
+//     handleClick = () =>{
+//         console.log("Button Clicked", this);
+//     }
+//     render(){
+//         return(
+//             <div>
+//                 <h1> Hello {this.state.name} </h1>
+//                 <button onClick={this.handleClick}>Click Me</button>
+//                 </div>
+//         )
+//     }
+// }
+// export default Student;
+
+//Update setState
+
+import React, { Component } from 'react';
+class Student extends Component{
+constructor(props){
+    super(props);
+    this.state = {
+        name: "Umair",
+        roll: this.props.roll
+    };
+}
+
+    handleClick = () =>{
+        this.setState({name: "Jai"});
+    };
+    render(){
+        return(
+            <div>
+                <h1> Hello {this.state.name} Your Roll No. {this.props.roll} </h1>
+                <button onClick={this.handleClick}>Click Me</button>
+                </div>
+        )
+    }
+}
+export default Student;
+
